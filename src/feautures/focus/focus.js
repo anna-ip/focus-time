b27d60587332feb67ffb6c48aae7a9e70432c0d8
Anna <anna.almestal@tretton37.com>
2022-03-02 15:05:45 +0100

    changed failing styling

--- src/feautures/focus/focus.js
+++ src/feautures/focus/focus.js
@@ -1,28 +1,28 @@
-import React, { useState } from 'react';
-import { Text, View, StyleSheet, Platform } from 'react-native';
-import { TextInput } from 'react-native-paper';
-import { RoundedButton } from '../../components/RoundedButton';
-import { fontSizes, spacing } from '../../utils/sizes';
+import React, { useState } from "react";
+import { Text, View, StyleSheet, Platform } from "react-native";
+import { TextInput } from "react-native-paper";
+import { RoundedButton } from "../../components/RoundedButton";
+import { fontSizes, spacing } from "../../utils/sizes";
 
 export const Focus = ({ addSubject }) => {
   const [subject, setSubject] = useState();
   return (
     <View style={styles.container}>
-        <View style={styles.inputContainer}>
-          <TextInput
-            label="What would you like to focus on?"
-            style={styles.textInput}
-            onChangeText={setSubject}
-          />
-          <View style={styles.button}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          label="What would you like to focus on?"
+          style={styles.textInput}
+          onChangeText={setSubject}
+        />
+        <View style={styles.button}>
           <RoundedButton
             size={50}
             title="+"
             onPress={() => addSubject(subject)}
           />
-          </View>
         </View>
       </View>
+    </View>
   );
 };
 
@@ -31,15 +31,16 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   inputContainer: {
-    justifyContent: 'top',
+    justifyContent: "flex-start",
     padding: spacing.lg,
-    flexDirection: 'row',
+    flexDirection: "row",
+    ß,
   },
   textInput: {
     flex: 1,
     marginRight: spacing.lg,
   },
   button: {
-    justifyContent: 'center'
-  }
+    justifyContent: "center",
+  },
 });
